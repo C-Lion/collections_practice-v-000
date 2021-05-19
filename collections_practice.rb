8ccd71554e467d74f83ba7257db766698fd71d70
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-19 18:48:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,8 +50,8 @@ end
 
 #see if you can use .each_with_index in addition to .collect to solve this one
 def add_s(array)
-  array.each_with_index.collect do |word, index|
-    unless index == 1 
+  array.each_with_index do |word, index|
+    if index == 1 
       array[index] = "#{array}s"
     end
     
